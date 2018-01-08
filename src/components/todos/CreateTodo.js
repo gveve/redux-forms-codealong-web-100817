84c26fb875b7e40c6efd354bd493ace764d7e2ed
github username <github email address>
2018-01-08 16:40:37 -0500

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
-  render() {
+  render(){
     return(
       <div>
-        Create Todo Component
+        <form>
+          <p>
+            <label>add todo</label>
+            <input type="text" onChange={(event) => this.handleChange(event)}/>
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
-    )
+    );
   }
+ 
 }
 
 export default CreateTodo;
